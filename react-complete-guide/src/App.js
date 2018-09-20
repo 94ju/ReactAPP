71574a7f58f7febf3ea5cc18cb4ab3a7fe375d27
94ju <janithherath94@gmail.com>
2018-09-20 12:20:42 +0530

    Dynamic content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    return (
+   return(
       <div className="App">
         <h1>Hi react </h1>
+        <Person/> 
       </div>
-    );
+    )
+      // return React.createElement('div',{className:'App'},React.createElement('h1',null,'react app'))
+   
   }
 }
 
