744d603b94e5527cee5c3d28147a3a9653dcf1ad
94ju <janithherath94@gmail.com>
2018-09-25 10:42:59 +0530

    JS states

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,12 +4,28 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state ={
+    person : [
+      {name: "janith" ,age :24},
+      {name: "udara" ,age :31}
+    ]
+  }
+  switchNameHandler =() => {
+
+    this.setState({
+      person : [
+        {name: "Anil Herath" ,age :42},
+        {name: "udara" ,age :31}
+      ]
+    })
+  }
   render() {
    return(
       <div className="App">
         <h1>Hi react </h1>
-        <Person name ="Janith" age = "18"/> 
-        <Person>My Hobby</Person>
+        <button onClick={this.switchNameHandler}>Switch Button</button>
+        <Person name ={this.state.person[0].name}  age ={this.state.person[0].age}/> 
+        <Person  name ={this.state.person[1].name}  age ={this.state.person[1].age}>My Hobby</Person>
       </div>
     )
       
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const person =(props ) =>{
     return (
         <div>
-            <p>I am {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
+            <p>I am {props.name} and I am {props.age} years old {props.children}</p>
+  
+           
         </div>
         ) 
 };
