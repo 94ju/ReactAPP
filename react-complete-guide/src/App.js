c756bd2cd237b850146988e4b35c2c60c9fb3a64
94ju <janithherath94@gmail.com>
2018-09-27 16:40:26 +0530

    test

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,75 +5,71 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  state ={
-    person : [
-      {name: "janith" ,age :24},
-      {name: "udara" ,age :31}
-    ],
-    showPerson:false
-  }
-
- namechangeHandler =(event) => {
-
-    this.setState({
-      person : [
-        {name: event.target.value ,age :422},
-        {name: "udara" ,age :31}
-      ]
-    })
-  }
-  deletePersonHandler =(personIndex) =>{
-      const person =this.state.person;
-      person.splice(personIndex);
-      this.setState({person:person})
-  }
-  tooglePersonHandler=()=>{
-      console.log("check");
-      const doesshow =this.state.showPerson;
-      this.setState({showPerson: !doesshow});
-  }
-  render() {
-    const style ={
-      backgroundColor : 'red',
-      font: 'inherit',
-      border :'1px solid blue',
-      padding : '8px',
-      cursor: 'pointer'
-      
-    };
-    let person = null;
-    if(this.state.showPerson){
-      person =(    
-      <div>
-        {
-          this.state.person.map((persons,index)=>{
-          return <Person
-          click = {()=>this.deletePersonHandler(index)}
-          name={persons.name}
-          age={persons.age}/>
-        })}
-        {/* <Person name ="aa"  age ={this.state.person[0].age}/> 
-        
-        <Person click={this.switchNameHandler} 
-                onIn={this.namechangeHandler}
-                name ={this.state.person[0].name} 
-                age ={this.state.person[1].age}>My Hobby</Person>
-        <Person name="Aruna" age="24"></Person> */}
-      </div>)
-      style.backgroundColor='red';
-    }
-   return(
-      <div className="App">
-        <h1>Hi react </h1>
-        <button style={style} onClick={this.tooglePersonHandler}>Toogle Persons</button>
-      { 
-          person 
+      state ={
+        person : [
+          {name: "janith" ,age :24},
+          {name: "udara" ,age :31}
+        ],
+        showPerson:false
       }
- 
-      </div>
-    )
+      namechangeHandler =(event) => {
+          this.setState({
+            person : [
+              {name: event.target.value ,age :422},
+              {name: "udara" ,age :31}
+            ]
+          })
+      }
+      deletePersonHandler =(personIndex) =>{
+            const person =this.state.person;
+            person.splice(personIndex);
+            this.setState({person:person})
+      }
+      tooglePersonHandler=()=>{
+            const doesshow =this.state.showPerson;
+            this.setState({showPerson: !doesshow});
+      }
+      render() {
+          const style ={
+            backgroundColor : 'red',
+            font: 'inherit',
+            border :'1px solid blue',
+            padding : '8px',
+            cursor: 'pointer'
+          };
+          let person = null;
+          if(this.state.showPerson){
+                person =(    
+                <div>
+                  {
+                    this.state.person.map((persons,index)=>{
+                    return <Person
+                    click = {()=>this.deletePersonHandler(index)}
+                    name={persons.name}
+                    age={persons.age}/>
+                  })}
+                  {/* <Person name ="aa"  age ={this.state.person[0].age}/> 
+                  
+                  <Person click={this.switchNameHandler} 
+                          onIn={this.namechangeHandler}
+                          name ={this.state.person[0].name} 
+                          age ={this.state.person[1].age}>My Hobby</Person>
+                  <Person name="Aruna" age="24"></Person> */}
+                </div>)
+                style.backgroundColor='red';
+          }
+          return(
+              <div className="App">
+                <h1>Hi react </h1>
+                <button style={style} onClick={this.tooglePersonHandler}>Toogle Persons</button>
+                { 
+                  person 
+                }
+        
+              </div>
+          )
       
-  }
+      }
 }
 
 export default App;
