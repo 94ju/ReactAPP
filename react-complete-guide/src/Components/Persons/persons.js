411bf40dfcb009b96c38f161fe74f65ae5b018bd
94ju <janithherath94@gmail.com>
2018-10-06 23:25:57 +0530

    Statefull components

--- react-complete-guide/src/Components/Persons/persons.js
+++ react-complete-guide/src/Components/Persons/persons.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React,{Component} from 'react'
 import Person from './Person/Person'
-const persons = (propes) => propes.persons.map((person ,index) =>{
-    return  <Person
-        click ={()=>propes.clicked(index)}
-        name = {person.name}
-        age = {person.age}
-        key ={person.id}
-        change={(event)=>propes.changed(event , person.id)}
-    />
- });
-export default persons;
+
+
+class Persons extends Component{
+    render(){
+        return this.props.persons.map((person ,index) =>{ 
+            return <Person
+                click ={()=>this.props.clicked(index)}
+                name = {person.name}
+                age = {person.age}
+                key ={person.id}
+                change={(event)=>this.props.changed(event , person.id)} />
+
+        }); 
+    }      
+}
+export default Persons;
+
